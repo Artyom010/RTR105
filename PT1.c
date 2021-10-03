2ca78d80235cfd1a0bcbfae66101d662fe595f76
Artyom010 <73239547+Artyom010@users.noreply.github.com>
2021-10-03 21:24:59 +0300

    Add files via upload

---
+++ PT1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+printf("Ievadiet 2 skaitlus:\n");
+
+long int pirmais, otrais;
+
+scanf("%ld",&pirmais);
+scanf("%ld",&otrais);
+
+printf("Rezultats: %ld",pirmais*otrais);
+return 0;
+
+}
